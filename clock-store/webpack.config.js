41ef703eb60d4f6effe6e04b1b80287fd6affcd2
Maksim <xnagpa@outlook.com>
2020-02-08 19:29:09 +0300

    Adds stylelint

--- clock-store/webpack.config.js
+++ clock-store/webpack.config.js
@@ -24,14 +24,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(woff2?|ttf|otf|eot|svg)$/,
-      //   exclude: /node_modules/,
-      //   loader: 'file-loader',
-      //   options: {
-      //       name: '[path][name].[ext]'
-      //   }
-      // },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
