5074cc837e9b38479113d7d79c731d2eb51e384c
Maksim <m.glinka@rambler-co.ru>
2020-02-26 14:09:58 +0300

    Improves js formatting

--- clock-store/src/javascripts/app.js
+++ clock-store/src/javascripts/app.js
@@ -1,5 +1,6 @@
 const mainMenuButton = document.querySelector('.js-show-main-menu');
 const mainMenu =  document.querySelector('.js-main-menu');
+
 mainMenuButton.addEventListener('click', () => {
   mainMenu.classList.toggle('visuallyhidden');
 });
