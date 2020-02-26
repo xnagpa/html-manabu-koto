debeb3af81f20f3cadc79b6a0ab4bf43e6a354df
Maksim <m.glinka@rambler-co.ru>
2020-02-26 15:58:51 +0300

    Adds proper close button

--- clock-store/src/javascripts/app.js
+++ clock-store/src/javascripts/app.js
@@ -1,6 +1,12 @@
 const mainMenuButton = document.querySelector('.js-show-main-menu');
+const logo = document.querySelector('.js-logo');
+const hamburger = document.querySelector('.js-hamburger');
+const close = document.querySelector('.js-close');
 const mainMenu =  document.querySelector('.js-main-menu');
 
 mainMenuButton.addEventListener('click', () => {
   mainMenu.classList.toggle('visuallyhidden');
+  logo.classList.toggle('visuallyhidden');
+  close.classList.toggle('visuallyhidden');
+  hamburger.classList.toggle('visuallyhidden');
 });
