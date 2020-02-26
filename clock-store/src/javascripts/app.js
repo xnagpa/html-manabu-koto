32b58fa94627996d4bc469a0b1558d22978b1417
Maksim <m.glinka@rambler-co.ru>
2020-02-26 14:09:08 +0300

    Removes DOMContentLoaded event handler

--- clock-store/src/javascripts/app.js
+++ clock-store/src/javascripts/app.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const mainMenuButton = document.querySelector('.js-show-main-menu');
-  const mainMenu =  document.querySelector('.js-main-menu');
-  mainMenuButton.addEventListener('click', () => {
-    mainMenu.classList.toggle('visuallyhidden');
-  });
+const mainMenuButton = document.querySelector('.js-show-main-menu');
+const mainMenu =  document.querySelector('.js-main-menu');
+mainMenuButton.addEventListener('click', () => {
+  mainMenu.classList.toggle('visuallyhidden');
 });
