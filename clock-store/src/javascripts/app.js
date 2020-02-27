b2948d8bcf2452f34888caeb8f997a342e5184a9
Maksim <m.glinka@rambler-co.ru>
2020-02-27 15:36:19 +0300

    Prettify js, hidden saga

--- clock-store/src/javascripts/app.js
+++ clock-store/src/javascripts/app.js
@@ -3,11 +3,9 @@ const mainMenuButton = document.querySelector('.js-show-main-menu-button');
 
 const hamburgerIcon = mainMenuButton.querySelector('.js-hamburger-icon');
 const closeIcon = mainMenuButton.querySelector('.js-close-icon');
-const logoIcon = document.querySelector('.js-logo-icon');
 
 mainMenuButton.addEventListener('click', () => {
   mainMenu.classList.toggle('visuallyhidden');
-  logoIcon.classList.toggle('js-main-nav--hidden');
-  closeIcon.classList.toggle('js-main-nav--hidden');
-  hamburgerIcon.classList.toggle('js-main-nav--hidden');
+  closeIcon.classList.toggle('hidden');
+  hamburgerIcon.classList.toggle('hidden');
 });
