ddbac6e1befbdcf4398c4b0c4ecda396a4c2598b
Maksim <m.glinka@rambler-co.ru>
2020-02-27 15:29:43 +0300

    Prettify js

--- clock-store/src/javascripts/app.js
+++ clock-store/src/javascripts/app.js
@@ -1,12 +1,13 @@
-const mainMenuButton = document.querySelector('.js-show-main-menu');
-const logo = document.querySelector('.js-logo');
-const hamburger = document.querySelector('.js-hamburger');
-const close = document.querySelector('.js-close');
 const mainMenu =  document.querySelector('.js-main-menu');
+const mainMenuButton = document.querySelector('.js-show-main-menu-button');
+
+const hamburgerIcon = mainMenuButton.querySelector('.js-hamburger-icon');
+const closeIcon = mainMenuButton.querySelector('.js-close-icon');
+const logoIcon = document.querySelector('.js-logo-icon');
 
 mainMenuButton.addEventListener('click', () => {
   mainMenu.classList.toggle('visuallyhidden');
-  logo.classList.toggle('visuallyhidden');
-  close.classList.toggle('visuallyhidden');
-  hamburger.classList.toggle('visuallyhidden');
+  logoIcon.classList.toggle('js-main-nav--hidden');
+  closeIcon.classList.toggle('js-main-nav--hidden');
+  hamburgerIcon.classList.toggle('js-main-nav--hidden');
 });
