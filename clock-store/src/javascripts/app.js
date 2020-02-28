2e3aed55ad41d6be47651a8b844da6e29c5c42c6
Maksim <m.glinka@rambler-co.ru>
2020-02-28 16:06:06 +0300

    Improves js structure

--- clock-store/src/javascripts/app.js
+++ clock-store/src/javascripts/app.js
@@ -1,8 +1,9 @@
-const mainMenu =  document.querySelector('.js-main-nav--minimized');
-const mainMenuList =  document.querySelector('.js-main-menu-list');
 const mainMenuToggler = document.querySelector('.js-main-menu-toggler');
 
 function toggleMainMenuVisibility() {
+  const mainMenu =  document.querySelector('.js-main-nav');
+  const mainMenuList =  document.querySelector('.js-main-menu-list');
+
   mainMenu.classList.toggle('js-main-nav--minimized');
   mainMenuList.classList.toggle('visuallyhidden');
 }
