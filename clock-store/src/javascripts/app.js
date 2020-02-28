28c9ea92a11b75378cd735351174070b81d9f01d
Maksim <m.glinka@rambler-co.ru>
2020-02-28 15:52:05 +0300

    Refactors js

--- clock-store/src/javascripts/app.js
+++ clock-store/src/javascripts/app.js
@@ -2,8 +2,14 @@ const mainMenu =  document.querySelector('.js-main-nav--minimized');
 const mainMenuList =  document.querySelector('.js-main-menu-list');
 const mainMenuToggler = document.querySelector('.js-main-menu-toggler');
 
-mainMenuToggler.addEventListener('click', () => {
-  //Hides main menu
+function toggleMainMenuVisibility() {
   mainMenu.classList.toggle('js-main-nav--minimized');
   mainMenuList.classList.toggle('visuallyhidden');
+}
+
+mainMenuToggler.addEventListener('click', () => {
+  toggleMainMenuVisibility();
 });
+
+
+
